44ebf8c748079206c9a9512ced7bb1d28e6f0503
Bisma Ahmed <bahmed0@chicagobooth.edu>
2017-07-26 22:39:33 +0000

    latlong

--- app/controllers/meteorologist_controller.rb
+++ app/controllers/meteorologist_controller.rb
@@ -7,7 +7,7 @@ class MeteorologistController < ApplicationController
   end
 
   def street_to_weather
-    @street_address = params[:user_street_address]
+    @street_address = params[:user_street_address].to_s
 
     # ==========================================================================
     # Your code goes below.
@@ -15,17 +15,27 @@ class MeteorologistController < ApplicationController
     # The street address that the user typed is in the variable @street_address.
     # ==========================================================================
 
+    street_address_nospaces = @street_address.gsub(" ","+")
+    url = "https://maps.googleapis.com/maps/api/geocode/json?address="+street_address_nospaces
+    parsed_data = JSON.parse(open(url).read)
+    
+    @latitude = parsed_data["results"]["0"]["geometry"]["location"]["lat"]
+
+    @longitude = parsed_data["results"]["0"]["geometry"]["location"]["lng"]
+    
+    url="https://api.darksky.net/forecast/db79463d0817e86e5a4d206d90eac787/"+@lat.to_s+","+@lng.to_s
+    parsed_data = JSON.parse(open(url).read)
 
 
-    @current_temperature = "Replace this string with your answer."
+    @current_temperature = parsed_data["currently"]["temperature"]
 
-    @current_summary = "Replace this string with your answer."
+    @current_summary = parsed_data["currently"]["summary"]
 
-    @summary_of_next_sixty_minutes = "Replace this string with your answer."
+    @summary_of_next_sixty_minutes = parsed_data["minutely"]["summary"]
 
-    @summary_of_next_several_hours = "Replace this string with your answer."
+    @summary_of_next_several_hours = parsed_data["hourly"]["summary"]
 
-    @summary_of_next_several_days = "Replace this string with your answer."
+    @summary_of_next_several_days = parsed_data["daily"]["summary"]
 
     render("meteorologist/street_to_weather.html.erb")
   end
