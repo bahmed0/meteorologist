9c1f544598f2682133368f3f7c89583ae593bac1
Bisma Ahmed <bahmed0@chicagobooth.edu>
2017-08-23 22:24:19 +0000

    final

--- app/controllers/geocoding_controller.rb
+++ app/controllers/geocoding_controller.rb
@@ -18,8 +18,8 @@ class GeocodingController < ApplicationController
     street_address_nospaces=@street_address.gsub(" ","+")
     url = "https://maps.googleapis.com/maps/api/geocode/json?address="+street_address_nospaces
     parsed_data = JSON.parse(open(url).read)
-    @lat = parsed_data["results"][0]["geometry"]["location"]["lat"]
-    @lng = parsed_data["results"][0]["geometry"]["location"]["lng"]
+    @latitude = parsed_data["results"][0]["geometry"]["location"]["lat"]
+    @longitude = parsed_data["results"][0]["geometry"]["location"]["lng"]
 
     render("geocoding/street_to_coords.html.erb")
   end
