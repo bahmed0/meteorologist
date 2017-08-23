e641f313a7931bb80078dc7eac185dffb48685a5
Bisma Ahmed <bahmed0@chicagobooth.edu>
2017-08-23 22:18:18 +0000

    think im done

--- app/controllers/meteorologist_controller.rb
+++ app/controllers/meteorologist_controller.rb
@@ -7,7 +7,7 @@ class MeteorologistController < ApplicationController
   end
 
   def street_to_weather
-    @street_address = params[:user_street_address].to_s
+    @street_address = params[:user_street_address]
 
     # ==========================================================================
     # Your code goes below.
@@ -18,24 +18,23 @@ class MeteorologistController < ApplicationController
     street_address_nospaces = @street_address.gsub(" ","+")
     url = "https://maps.googleapis.com/maps/api/geocode/json?address="+street_address_nospaces
     parsed_data = JSON.parse(open(url).read)
+    @lat = parsed_data["results"][0]["geometry"]["location"]["lat"]
+    @lng = parsed_data["results"][0]["geometry"]["location"]["lng"]
     
-    @latitude = parsed_data["results"]["0"]["geometry"]["location"]["lat"]
-
-    @longitude = parsed_data["results"]["0"]["geometry"]["location"]["lng"]
-    
-    url="https://api.darksky.net/forecast/db79463d0817e86e5a4d206d90eac787/"+@lat.to_s+","+@lng.to_s
-    parsed_data = JSON.parse(open(url).read)
+    darksky_url="https://api.darksky.net/forecast/db79463d0817e86e5a4d206d90eac787/"+@lat.to_s+","+@lng.to_s
+    darksky_parsed_data = JSON.parse(open(darksky_url).read)
+  
 
 
-    @current_temperature = parsed_data["currently"]["temperature"]
+    @current_temperature = darksky_parsed_data["currently"]["temperature"]
 
-    @current_summary = parsed_data["currently"]["summary"]
+    @current_summary = darksky_parsed_data["currently"]["summary"]
 
-    @summary_of_next_sixty_minutes = parsed_data["minutely"]["summary"]
+    @summary_of_next_sixty_minutes = darksky_parsed_data["minutely"]["summary"]
 
-    @summary_of_next_several_hours = parsed_data["hourly"]["summary"]
+    @summary_of_next_several_hours = darksky_parsed_data["hourly"]["summary"]
 
-    @summary_of_next_several_days = parsed_data["daily"]["summary"]
+    @summary_of_next_several_days = darksky_parsed_data["daily"]["summary"]
 
     render("meteorologist/street_to_weather.html.erb")
   end
